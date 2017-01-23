c180868f72dc5d238fcc9fe5c4891bf377ea8621
Philip <Philip.zdb@gmail.com>
2017-01-23 16:40:01 +0100

    Adds rspec tests for attribute validation (failing)

--- spec/models/robot_spec.rb
+++ spec/models/robot_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe Robot, type: :model do
     it { is_expected.to have_db_column :jetpack }
   end
 
+  describe "Validations" do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :type }
+    it { is_expected.to validate_presence_of :armor }
+    it { is_expected.to validate_presence_of :jetpack }
+  end
+
 end
