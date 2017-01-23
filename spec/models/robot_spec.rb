f5e5ef3ddce0a374f3f894fe67bcf66b79f5a9f0
Philip <Philip.zdb@gmail.com>
2017-01-23 16:00:32 +0100

    Adds initial rspec test for Robot model

--- spec/models/robot_spec.rb
+++ spec/models/robot_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Robot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "DB tables" do
+    it { is_expected.to have_db_column :name }
+    it { is_expected.to have_db_column :type }
+    it { is_expected.to have_db_column :armor }
+    it { is_expected.to have_db_column :jetpack }
+  end
+
 end
