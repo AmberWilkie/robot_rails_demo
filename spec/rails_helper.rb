d195083636c1b08ebf49ef2ee4cf8d3fb63c0d08
Philip <Philip.zdb@gmail.com>
2017-01-23 16:17:33 +0100

    Adds coveralls to spechelper instead of rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,8 +3,6 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'coveralls'
-Coveralls.wear!('rails')
 
 ActiveRecord::Migration.maintain_test_schema!
 
